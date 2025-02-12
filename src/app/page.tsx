5223474c98b291a097fe6e9182bb3ac57391615c
FL232323 <Fosterleboeuf23@gmail.com>
2025-02-12 15:53:48 -0500

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { BettingDashboard } from '@/components/betting-dashboard/betting-dashboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 md:p-8">
+      <BettingDashboard />
+    </main>
+  );
+}
